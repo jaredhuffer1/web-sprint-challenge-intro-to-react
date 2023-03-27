98cee84932419fbc163a99825e8c5adcf488afff
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-03-27 18:28:44 -0400

    this is hard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Character from './Character';
 
-const App = () => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+
+
+const App = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const response = await fetch('https://swapi.dev/api/people/');
+      const data = await response.json();
+      setCharacters(data.results);
+    };
+
+    fetchCharacters();
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((character) => (
+        <Character key={character.name} character={character} />
+        ))}
     </div>
+    
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+function Character(props) {
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <p>Gender: {props.gender}</p>
+        <p>Birth Year: {props.birthYear}</p>
+        <p>Height: {props.height} cm</p>
+        <p>Mass: {props.mass} kg</p>
+      </div>
+    );
+  }
+  export default Character;
+  
