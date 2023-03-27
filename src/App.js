dfb73c15ea226e76ad2ca1715e01e7b6c527c345
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-03-27 19:58:49 -0400

    not sure what i did

--- src/App.js
+++ src/App.js
@@ -1,35 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import Character from './components/Character';
 import './App.css';
-import { data } from './mocks/handlers'; 
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
+const server = setupServer(
+  rest.get('https://swapi.dev/api/people/', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          name: 'Luke Skywalker',
+          height: '172',
+          mass: '77',
+          hair_color: 'blond',
+          skin_color: 'fair',
+          eye_color: 'blue',
+          birth_year: '19BBY',
+          gender: 'male',
+          homeworld: 'http://swapi.dev/api/planets/1/',
+          films: [
+            'A New Hope',
+            'The Empire Strikes Back',
+            'Return of the Jedi',
+            'Revenge of the Sith',
+          ],
+          species: [],
+          vehicles: [
+            'http://swapi.dev/api/vehicles/14/',
+            'http://swapi.dev/api/vehicles/30/',
+          ],
+          starships: [
+            'http://swapi.dev/api/starships/12/',
+            'http://swapi.dev/api/starships/22/',
+          ],
+          created: '2014-12-09T13:50:51.644000Z',
+          edited: '2014-12-20T21:17:56.891000Z',
+          url: 'http://swapi.dev/api/people/1/',
+        },
+      ])
+    );
+  })
+);
 
+server.listen();
 
+const App = () => {
+  const [characters, setCharacters] = useState([]);
 
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  useEffect(() => {
+    fetch('https://swapi.dev/api/people/')
+      .then((res) => res.json())
+      .then((data) => setCharacters(data))
+      .catch((error) => console.error('Error:', error));
+  }, []);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  
-  
-  const App = () => {
-    const [characters, setCharacters] = useState([]);
-  
-    useEffect(() => {
-      setCharacters(data);
-    }, []);
-  
-    return (
-      <div className="App">
-        <h1 className="Header">Characters</h1>
-        {characters && characters.map((character) => (
+  return (
+    <div className="App">
+      <h1 className="Header">Characters</h1>
+      {characters &&
+        characters.map((character) => (
           <Character key={character.name} character={character} />
         ))}
-      </div>
-    );
-  };
-  
-  export default App;
-  
+    </div>
+  );
+};
+
+export default App;
