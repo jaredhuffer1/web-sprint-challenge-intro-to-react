9b79965d137e7e4067b7ae50f064f2db8a57ad01
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-03-27 19:13:59 -0400

    ?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Character from './components/Character';
+import './App.css';
+
 
 
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,17 @@
-// Write your Character component here
 import React from 'react';
-function Character(props) {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Gender: {props.gender}</p>
-        <p>Birth Year: {props.birthYear}</p>
-        <p>Height: {props.height} cm</p>
-        <p>Mass: {props.mass} kg</p>
-      </div>
-    );
-  }
-  export default Character;
+
+function Character({ character }) {
+  return (
+    <div>
+      <h2>{character.name}</h2>
+      <p>Gender: {character.gender}</p>
+      <p>Birth Year: {character.birthYear}</p>
+      <p>Height: {character.height} cm</p>
+      <p>Mass: {character.mass} kg</p>
+    </div>
+  );
+}
+
+export default Character;
+
   
